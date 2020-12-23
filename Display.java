dd0488279c54e3d5139dc35cd547c76e8e88beac
slavagg <reicode@ya.ru>
2020-12-23 15:02:47 +0300

    Added more functional

--- Display.java
+++ Display.java
@@ -1,51 +1,86 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.datatransfer.*;
 import java.util.Random;
 
 public class Display extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -118707512687693000L;
 	JTextField pwdtf = new JTextField(15);
 	JTextField pwdlbl = new JTextField(15);
+	JLabel pwdlbl2 = new JLabel("Generated password:");
 	JTextField pwdalph = new JTextField(15);
 	JButton genbtn = new JButton("Generate");
+	JButton cpbtn = new JButton("Copy");
 	JLabel lbl = new JLabel("Enter number of symbols:");
-	JLabel lblal = new JLabel("Enter alphabet:");
+	JLabel lblal = new JLabel("Enter custom alphabet:");
+	JRadioButton numrb = new JRadioButton("Numbers alphabet");
+	JRadioButton defrb = new JRadioButton("Letters&numbers alphabet");
+	JRadioButton extrb = new JRadioButton("Extended alphabet");
+	ButtonGroup group = new ButtonGroup();
+	JPanel emptpane = new JPanel();
 	
 	public Display(String s) {
 		super(s);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		GridLayout layout = new GridLayout(3, 2, 1, 1);
+		GridLayout layout = new GridLayout(4, 3, 1, 1);
 		setLayout(layout);
+		group.add(numrb);
+		group.add(defrb);
+		group.add(extrb);
 		add(lbl);
 		add(pwdtf);
+		add(numrb);
 		add(lblal);
 		add(pwdalph);
+		add(defrb);
+		add(pwdlbl2);
 		add(pwdlbl);
+		add(extrb);
+		add(emptpane);
+		add(cpbtn);
 		add(genbtn);
-        
 		pwdlbl.setText("Password");
-		pwdalph.setText("1234567890qwertyuiopasdfghjklzxcvbnm!?&%QWERTYUIOPASDFGHJKLZXCVBNM");
-		ActionListener actionListener = new List();
-        
-		genbtn.addActionListener(actionListener);
 		
+		ActionListener actionListener = new List();
+		genbtn.addActionListener(actionListener);
+		cpbtn.addActionListener(actionListener);
 	}
 	public class List implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-            String pwd = "";
-            Random r = new Random();
-            String alphabet = pwdalph.getText();
-			try {
-            	int i = Integer.parseInt(pwdtf.getText());
-            	for(int j = 0; j != i; j++) {
-            		pwd = pwd + alphabet.charAt(r.nextInt(alphabet.length()));
+            if(e.getSource() == genbtn) {
+            	if(numrb.isSelected()) {
+            		pwdalph.setText("1234567890");
+            	}else if(defrb.isSelected()) {
+            		pwdalph.setText("1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM");
+            	}else if(extrb.isSelected()) {
+            		pwdalph.setText("1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM?!@#$%^&*<>.,");
             	}
-            	pwdlbl.setText(pwd);
-            }catch (NumberFormatException nfe)
-            {
-            	JOptionPane.showMessageDialog(null, "NumberFormatException\n" + nfe + "\n\nEnter integer number!");
-              }
+            	String pwd = "";
+            	Random r = new Random();
+            	String alphabet = pwdalph.getText();
+				try {
+            		int i = Integer.parseInt(pwdtf.getText());
+            		for(int j = 0; j != i; j++) {
+            			pwd = pwd + alphabet.charAt(r.nextInt(alphabet.length()));
+            		}
+            		pwdlbl.setText(pwd);
+            	}catch (NumberFormatException nfe){
+            		JOptionPane.showMessageDialog(null, "NumberFormatException\n" + nfe + "\n\nEnter integer number!");
+            	}catch (IllegalArgumentException iae){
+            		JOptionPane.showMessageDialog(null, "IllegalArgumentException\n" + iae + "\n\nEnter password alphabet or select pattern!");
+            	}
+            }
+            if(e.getSource() == cpbtn) {
+            	String get = pwdlbl.getText();
+            	StringSelection selec= new StringSelection(get);
+            	Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            	clipboard.setContents(selec, null);
+            }
         }
 	}
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		Display win = new Display("ASD OpenPasswGen");
 		win.setVisible(true);
-		win.setSize(400, 110);
+		win.setSize(700, 115);
 	}
 }
